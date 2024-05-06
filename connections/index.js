a0e0334def1744d0eb285605b05acfa9271e7a27
HannahWu <a22483268@gmail.com>
2024-05-06 21:07:24 +0800

    feat: 增加dev env

--- connections/index.js
+++ connections/index.js
@@ -2,10 +2,14 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 
 dotenv.config({ path: './config.env' })
-
-const DB = process.env.DATABASE.replace(
-    '<password>',
-    process.env.DATABASE_PASSWORD
-)
+let DB
+if (process.env.NODE_ENV === 'dev') {
+    DB = process.env.DATABASE_LOCAL
+} else {
+    DB = process.env.DATABASE.replace(
+        '<password>',
+        process.env.DATABASE_PASSWORD
+    )
+}
 
 mongoose.connect(DB).then(() => console.log('資料庫連線成功'))
