1e337b7e29270c9cd58223763b643fce2d981f4f
HannahWu <a22483268@gmail.com>
2024-06-13 23:07:35 +0800

    fix: 上傳圖片大小過大改為自定義錯誤

--- middlewares/uploadImage.js
+++ middlewares/uploadImage.js
@@ -7,6 +7,13 @@ const uploadImage = multer({
         fileSize: 2 * 1024 * 1024, // 不超過2mb
     },
     fileFilter(req, file, cb) {
+        const fileSize = parseInt(req.headers['content-length'])
+
+        if (fileSize > 2 * 1024 * 1024) {
+            // 自定義檔案大小錯誤
+            cb(new Error('檔案大小超過 2MB 的限制。'))
+        }
+
         const ext = path.extname(file.originalname).toLowerCase() // 取得副檔名
         if (ext !== '.jpg' && ext !== '.png' && ext !== '.jpeg') {
             cb(new Error('檔案格式錯誤，僅限上傳 jpg， jpeg 與 png 格式。'))
